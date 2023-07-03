42233ff892e610561af482f92e105b48295d0e12
digidagsOrg <107912351+digidagsOrg@users.noreply.github.com>
2023-07-03 12:04:02 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,28 @@
+input.onButtonPressed(Button.AB, function () {
+    if (program == 0) {
+        if (stop == 0) {
+            radio.sendNumber(3)
+            stop = 1
+            basic.showLeds(`
+                # . . . #
+                # . . . #
+                # . . . #
+                # . . . #
+                # . . . #
+                `)
+        } else {
+            radio.sendNumber(33)
+            basic.showLeds(`
+                # # . # #
+                # # . # #
+                # # . # #
+                # # . # #
+                # # . # #
+                `)
+            stop = 0
+        }
+    }
+})
 input.onButtonPressed(Button.A, function () {
     if (program == 0) {
         if (stop == 0) {
@@ -27,39 +52,18 @@ function selectProgram () {
     basic.showIcon(IconNames.Square)
     while (true) {
         if (input.buttonIsPressed(Button.A)) {
+            basic.showString("A")
+            basic.pause(500)
             return 0
         } else {
             if (input.buttonIsPressed(Button.B)) {
+                basic.showString("B")
+                basic.pause(500)
                 return 1
             }
         }
     }
 }
-input.onButtonPressed(Button.AB, function () {
-    if (program == 0) {
-        if (stop == 0) {
-            radio.sendNumber(3)
-            stop = 1
-            basic.showLeds(`
-                # . . . #
-                # . . . #
-                # . . . #
-                # . . . #
-                # . . . #
-                `)
-        } else {
-            radio.sendNumber(33)
-            basic.showLeds(`
-                # # . # #
-                # # . # #
-                # # . # #
-                # # . # #
-                # # . # #
-                `)
-            stop = 0
-        }
-    }
-})
 input.onButtonPressed(Button.B, function () {
     if (program == 0) {
         if (stop == 0) {
@@ -87,7 +91,7 @@ input.onButtonPressed(Button.B, function () {
 })
 let stop = 0
 let program = 0
-selectProgram()
+let Pnumber = 0
 program = selectProgram()
 if (program == 0) {
     radio.setGroup(1)
@@ -95,11 +99,38 @@ if (program == 0) {
     basic.showIcon(IconNames.Heart)
 } else {
     if (program == 1) {
+        radio.setGroup(2)
         basic.showIcon(IconNames.Happy)
     }
 }
 basic.forever(function () {
     if (program == 1) {
-        wuKong.setMotorSpeed(wuKong.MotorList.M1, 100)
+        if (input.buttonIsPressed(Button.A) && Pnumber == 4) {
+            basic.showNumber(0)
+            Pnumber = 0
+        } else {
+            if (input.buttonIsPressed(Button.A) && Pnumber == 0) {
+                basic.showNumber(1)
+                Pnumber = 1
+            } else {
+                if (input.buttonIsPressed(Button.A) && Pnumber == 1) {
+                    basic.showNumber(2)
+                    Pnumber = 2
+                } else {
+                    if (input.buttonIsPressed(Button.A) && Pnumber == 2) {
+                        basic.showNumber(3)
+                        Pnumber = 3
+                    } else {
+                        if (input.buttonIsPressed(Button.A) && Pnumber == 3) {
+                            basic.showNumber(4)
+                            Pnumber = 4
+                        }
+                    }
+                }
+            }
+        }
+    }
+    if (input.buttonIsPressed(Button.B)) {
+        radio.sendNumber(Pnumber)
     }
 })
